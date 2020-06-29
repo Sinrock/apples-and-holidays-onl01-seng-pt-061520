a4717a99e3ea019c5d852278f7b5df6ac1080a47
Michael R <mruhaak@gmail.com>
2020-06-29 07:27:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -10,7 +10,7 @@ def second_supply_for_fourth_of_july(supplies_hash)
 end
 
 def add_supply_to_winter_holiday(supplies_hash, supply)
-  supplies_hash[:winter][:new_years] << supply
+  supplies_hash[:winter][:new_years] << "Balloons"
 end
 
 def add_supply_to_memorial_day(supplies_hash, supply)
